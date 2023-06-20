d9fb84e6db02da736146896a9036a72c3cab109a
F1st3k <N1k0s04k@gmail.com>
2023-06-20 11:07:49 +0100

    add data

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './components/Table/Table'
+import Table from "./components/Table/Table";
 
+let data: string[][] = [
+    ['Id', 'Name', 'Height', 'Mass', 'Gender', 'Url'],
+    ['1', 'Luke Skywalker', '172', '77', 'male', 'https://swapi.dev/api/people/1/'],
+    ['2', 'C-3PO', '167', '75', 'n/a', 'https://swapi.dev/api/people/2/'],
+    ['3', 'R2-D2', '96', '32', 'n/a', 'https://swapi.dev/api/people/3/'],
+];
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Table data={data}></Table>
       </header>
     </div>
   );

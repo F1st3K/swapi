1d0e848accee1ccf64ebb48a8492e84758d2a12b
F1st3k <N1k0s04k@gmail.com>
2023-06-21 09:30:01 +0100

    hook info peple

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,24 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './../App.css';
 import Table from "./../components/Table/Table";
-import People, {Person, toTableFormat} from "./../services/PeopleSwapi";
+import InfoPeople, {toTableFormat, Person} from "./../services/PeopleSwapi";
 
 let dataTable: string[][] = [];
 
 const Home = () => {
-    let [people, setPeople] = useState<Person[]>([]);
-    useEffect( () => {
-        People().then(data => {
-            const peopleData = data.results.map((person: Person) => ({
-                name: person.name,
-                height: person.height,
-                mass: person.mass,
-                hair_color: person.hair_color,
-                skin_color: person.skin_color,
-                eye_color: person.eye_color,
-                birth_year: person.birth_year,
-                gender: person.gender,
-            }));
-            setPeople(peopleData);
-            alert(peopleData);
-        });}, [])
+    const { data, loading } = InfoPeople();
+    const persons: Person[] = data ? data : [];
+    dataTable = toTableFormat(persons);
 
-    dataTable = toTableFormat(people);
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+    if (!data) {
+        return <div>No data available</div>;
+    }
     return (
-        <div className="App">
-                <Table data={dataTable}></Table>
+        <div>
+            <Table data={dataTable}></Table>
         </div>
     );
 }
--- src/services/PeopleSwapi.ts
+++ src/services/PeopleSwapi.ts
@@ -1,3 +1,5 @@
+import {useEffect, useState} from "react";
+
 export interface Person {
     name: string;
     height: string;
@@ -10,11 +12,29 @@ export interface Person {
 }
 
 export const PERSON_HEADERS = ['Name', 'Height', 'Mass', 'Hair color', 'Skin color', 'Eye color', 'Birth year', 'Gender'];
-export const InfoPeoples = () => {
-    return fetch('https://swapi.dev/api/people/')
-        .then(response => response.json())
-        .catch(error => console.error(error));
-}
+export const URL: string = 'https://swapi.dev/api/people/';
+
+export const InfoPeoples = ((): { data: Person[] | null; loading: boolean } => {
+    const [data, setData] = useState<Person[] | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(URL);
+                const json = await response.json();
+                setData(json.results);
+                setLoading(false);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return { data, loading };
+});
 
 export const InfoPeopleById = (id: number) => {
     return fetch('https://swapi.dev/api/people/' + id)
@@ -25,7 +45,7 @@ export const InfoPeopleById = (id: number) => {
 export const toTableFormat = (data: Person[]):string[][] => {
     let table: string[][] = [];
     table.push(PERSON_HEADERS);
-    for (const item of data) {
+    data.map((item, i) => {
         let row: string[] = [];
         row.push(item.name);
         row.push(item.height);
@@ -36,7 +56,8 @@ export const toTableFormat = (data: Person[]):string[][] => {
         row.push(item.birth_year);
         row.push(item.gender);
         table.push(row);
-    }
+        return row;
+    });
     return table;
 }
 

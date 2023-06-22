62e172d418cfce604bcbb1e9b1a8343d40146a14
F1st3k <N1k0s04k@gmail.com>
2023-06-22 23:10:45 +0100

    replace TablePeople

--- src/pages/ClientRoutes.tsx
+++ src/pages/ClientRoutes.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import Home from "./Home";
+import RouteTabs, {Tab} from "../components/RouteTabs/RouteTabs";
+import TablePeople from "./TablePeople";
 
 function ClientRoutes() {
+    const tabs: Tab[] = [{
+        title: 'peoples',
+        path: '/home/peoples',
+        component: TablePeople,
+    }];
     return (
-        <Routes>
-            <Route path='/home' Component={Home}/>
-        </Routes>
+        <div>
+            <Routes>
+                <Route path='/home/' Component={Home}/>
+            </Routes>
+            <RouteTabs tabs={tabs}/>
+        </div>
     );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,15 @@
-import React, {useState} from 'react';
-import './../App.css';
-import PagingTable from "../components/PagingTable/PagingTable";
-import InfoPeople, {toTableFormat} from "../services/PeopleSwapi";
+import React from 'react';
 import RouteTabs, {Tab} from "../components/RouteTabs/RouteTabs";
-
-let dataTable: string[][] = [];
+import TablePeople from "./TablePeople";
 
 const Home = () => {
-    const [currentPage, setCurrentPage] = useState<number>(1)
-    let { data, loading, countPage } = InfoPeople(currentPage);
-    const persons = data ? data : [];
-    dataTable = toTableFormat(persons);
     const tabs: Tab[] = [{
         title: 'd',
         path: '/home/d',
-        component: () => {return <div></div>}
+        component: TablePeople,
     }];
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-    if (!data) {
-        return <div>No data available</div>;
-    }
     return (
         <div>
-            <PagingTable data={dataTable} countPage={countPage/10} currentPage={currentPage}
-                         nextHandler={()=>{ setCurrentPage(currentPage + 1);}}
-                         prevHandler={()=>{ setCurrentPage(currentPage - 1);}}
-            />
             <RouteTabs tabs={tabs}/>
         </div>
     );

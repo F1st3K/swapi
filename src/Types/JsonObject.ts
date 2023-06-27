bd9165860eae32b91f47c85fd751530fd9a6407e
F1st3k <N1k0s04k@gmail.com>
2023-06-27 08:17:00 +0100

    complication type

--- src/Types/JsonObject.ts
+++ src/Types/JsonObject.ts
@@ -2,9 +2,17 @@ export type JsonValue = string | number | boolean | null;
 
 export type JsonArray = Array<JsonValue>;
 
-export type JsonObject = {
-    [key: string]: JsonValue | JsonArray | JsonObject
+export type JsonObject<T extends Record<string, JsonValue | JsonArray | JsonObject<T>>> = {
+    [key in keyof T]?: T[key] extends JsonValue | JsonArray
+        ? T[key]
+        : T[key] extends Record<string, JsonValue | JsonArray>
+            ? JsonObject<T[key]>
+            : never;
 };
+
+
+
+
 
 
 

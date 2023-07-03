0b4b8332be21a163938476f38a3a72242ba80428
F1st3k <N1k0s04k@gmail.com>
2023-07-03 14:50:40 +0100

    add view map

--- src/pages/Maps.tsx
+++ src/pages/Maps.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import {Box} from "@mui/material";
 import Home from "./Home";
+import {MapContainer, TileLayer} from "react-leaflet";
+import {LatLngExpression} from "leaflet";
 
 const Maps = () => {
     const homeTabs = Home("/maps")
+
+    const position: LatLngExpression = [51.505, -0.09]
     return (
         <>
             {homeTabs}
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            </Box>
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
         </>
     );
 }

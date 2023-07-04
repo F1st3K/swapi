c67dd92c1a198e82ad335c99dd76fd26fee117f3
F1st3k <N1k0s04k@gmail.com>
2023-07-04 09:01:34 +0100

    create hook use current geo position and use him

--- src/pages/Maps.tsx
+++ src/pages/Maps.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Home from "./Home";
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import {LatLngExpression} from "leaflet";
 import {Box} from "@mui/material";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import useCurrentGeoPosition, {GetPositionState} from "../Hooks/UseCurrentGeoPosition";
 
 const markerIcon = L.icon({
     iconUrl: 'marker-icon.png',
@@ -14,19 +15,26 @@ const markerIcon = L.icon({
 });
 
 const Maps = () => {
+    const [position, setPosition] = useState<LatLngExpression | null>(null);
+    let currentPosition: GetPositionState = useCurrentGeoPosition();
     const homeTabs = Home("/maps");
 
-    const position: LatLngExpression = [51.505, -0.09];
+    useEffect(() => {
+        if (currentPosition.data && !position) {
+            setPosition([currentPosition.data.coords.latitude, currentPosition.data.coords.longitude]);
+        }
+    }, [currentPosition])
+
     return (
         <>
             {homeTabs}
             <Box sx={style}>
-                <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{width: '100%', height: '100%'}}>
+                <MapContainer center={position ? position : [51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{width: '100%', height: '100%'}}>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={position} icon={markerIcon}>
+                    <Marker position={position ? position : [51.505, -0.09]} icon={markerIcon}>
                         <Popup>
                             A pretty CSS3 popup. <br /> Easily customizable.
                         </Popup>

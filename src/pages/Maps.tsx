09c7ee19a05e2e7a6ed2a937533cbe3b5683bb26
F1st3k <N1k0s04k@gmail.com>
2023-07-04 07:49:26 +0100

    add marker

--- src/pages/Maps.tsx
+++ src/pages/Maps.tsx
@@ -1,23 +1,57 @@
 import React from 'react';
 import Home from "./Home";
-import {MapContainer, TileLayer} from "react-leaflet";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import {LatLngExpression} from "leaflet";
+import {Box} from "@mui/material";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const markerIcon = L.icon({
+    iconUrl: 'marker-icon.png',
+    iconSize: [25, 41],
+    iconAnchor: [12.5, 41],
+    popupAnchor: [0, -41],
+});
 
 const Maps = () => {
-    const homeTabs = Home("/maps")
+    const homeTabs = Home("/maps");
 
-    const position: LatLngExpression = [51.505, -0.09]
+    const position: LatLngExpression = [51.505, -0.09];
     return (
         <>
             {homeTabs}
-            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-            </MapContainer>
+            <Box sx={style}>
+                <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{width: '100%', height: '100%'}}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={position} icon={markerIcon}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </Box>
         </>
     );
 }
+
+const style = {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '70%',
+    height: '70%',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+};
 
 export default Maps;
